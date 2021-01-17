6052487dcf1597a37b9fd217cb435ae9e4449f18
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2021-01-17 21:44:50 +0700

    add update

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -3,14 +3,30 @@ const slideUp =
     distance: '150%',
     origin: 'bottom',
     opacity: .2,
-    easing: 'ease-in-out'
+    easing: 'ease-in'
 };
+
+const fadeIn =
+{
+    distance: '0px',
+    opacity: .3,
+    easing: 'ease-in',
+    duration: 900
+};
+
 
 const item_slide_up = [
     document.querySelector('.heading-skill'),
     document.querySelector('.my-text1'),
     document.querySelector('.my-text2'),
     document.querySelector('.my-text3'),
+    document.querySelector('.btn-indigo'),
+    document.querySelector('.section-title'),
+    document.querySelector('.about-desc')
+];
+
+const item_fade_in = [
+    document.querySelector('.about-image'),
 
 ];
 
@@ -20,5 +36,22 @@ ScrollReveal()
         {
             delay: 200,
             reset: true
-        });
-ScrollReveal().reveal('.progress-bar', { delay: 100, easing: 'ease-in', duration: 1000 });
+        }
+    );
+
+ScrollReveal()
+    .reveal(
+        '.progress-bar',
+        {
+            delay: 100,
+            easing: 'ease-in',
+            duration: 1000
+        }
+    );
+
+
+
+ScrollReveal()
+    .reveal(
+        item_fade_in, fadeIn
+    )
